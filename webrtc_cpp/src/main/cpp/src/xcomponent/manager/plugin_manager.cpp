f06a6122d6891e92303b2d9498d38845caefd535
xyhedj <xyhedj@noreply.gitcode.com>
2025-11-04 14:40:46 +0800

    [source] 更新xcomponent部分代码和部分类构造方式

--- webrtc_cpp/src/main/cpp/src/xcomponent/manager/plugin_manager.cpp
+++ webrtc_cpp/src/main/cpp/src/xcomponent/manager/plugin_manager.cpp
@@ -37,11 +37,25 @@ PluginRender* PluginManager::GetPluginRender(int64_t& id)
     }
     return nullptr;
 }
-
+//
+//void* PluginManager::SetSurfaceId(int64_t surfaceId)
+//{
+//    OHNativeWindow *nativeWindow = nullptr;
+//    PluginRender *pluginRender = nullptr;
+//    if (windowMap_.find(surfaceId) == windowMap_.end()) {
+//        OH_NativeWindow_CreateNativeWindowFromSurfaceId(surfaceId, &nativeWindow);
+//        windowMap_[surfaceId] = nativeWindow;
+//    }
+//    if (pluginRenderMap_.find(surfaceId) == pluginRenderMap_.end()) {
+//        pluginRender = new PluginRender(surfaceId);
+//        pluginRenderMap_[surfaceId] = pluginRender;
+//    }
+//    return nullptr;
+//}
 void* PluginManager::SetSurfaceId(int64_t surfaceId)
 {
-    OHNativeWindow *nativeWindow = nullptr;
-    PluginRender *pluginRender = nullptr;
+    OHNativeWindow *nativeWindow;
+    PluginRender *pluginRender;
     if (windowMap_.find(surfaceId) == windowMap_.end()) {
         OH_NativeWindow_CreateNativeWindowFromSurfaceId(surfaceId, &nativeWindow);
         windowMap_[surfaceId] = nativeWindow;
@@ -50,6 +64,7 @@ void* PluginManager::SetSurfaceId(int64_t surfaceId)
         pluginRender = new PluginRender(surfaceId);
         pluginRenderMap_[surfaceId] = pluginRender;
     }
+    pluginRender->InitNativeWindow(nativeWindow);
     return nullptr;
 }
 
